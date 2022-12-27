6d9b9a27176814930a38b651e48529d972e47553
takato-noguchi <takato.noguchi23@gmail.com>
2022-12-27 17:05:43 +0900

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,73 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Welcome from '../views/WelcomePage'
+import ChatRoom from '../views/ChatRoom'
+import useValidate from '../auth/validate'
+
+const { error, validate } = useValidate()
+
+const requireAuth = async (to, from, next) => {
+
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+  
+  if (!uid || !client || !accessToken) {
+    console.log('ログインしていません')
+    next({ name:'Welcome' })
+    return
+  }
+  await validate()
+  if (error.value) {
+    console.log('認証に失敗しました')
+    next({ name: 'Welcome' })
+  } else {
+    next()
+  }
+  next()
+}
+
+const noRequireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid && !client && !accessToken) {
+    next()
+    return
+  }
+
+  await validate()
+
+  if (!error.value) {
+    next({ name: 'ChatRoom' })
+  } else {
+    next()
+  }
+}
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Welcome',
+    component: Welcome,
+    beforeEnter: noRequireAuth
+  },
+  {
+    path: '/chatroom',
+    name: 'ChatRoom',
+    component: ChatRoom,
+    beforeEnter: requireAuth
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
